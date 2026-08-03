0373c120af2e4aa1c248f2b78ad129dd7070ea1f
creator from The Dead Emipre <jacmil@poczta.fm>
2026-08-03 17:37:48 +0200

    Add files via upload

---
+++ Portfolio/main.py
@@ -0,0 +1,40 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# Uruchamianie strony z treścią
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Umiejętności dynamiczne
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+
+    return render_template(
+    'index.html', 
+    button_python=button_python,
+    button_discord=button_discord,
+    button_html=button_html,
+    button_db=button_db)
+
+def submit_form():
+    email = request.form.get('email')
+    text = request. form.get('text')
+    return render_template(
+    'index.html', 
+    email=email,
+    text=text)
+    with open('form.txt', 'a',) as f:
+        f.write(email + '\n' + text + '\n')
+
+if __name__ == "__main__":
+    app.run(debug=True)
